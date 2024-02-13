2f5c6f9c838881950f906f74f200bc0a67cef27f
SorinJunior <juninhosorin@gmail.com>
2024-02-13 19:09:57 -0300

    commit ex-4

---
+++ Exercicio-4/callback.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")();
+
+function adicao(){
+    let numero1 = Number(prompt("Digite o primero número:"));
+    let numero2 = Number(prompt("Digite o segundo número:"));
+
+    let soma = numero1 + numero2;
+    console.log("O resultado da soma é =", soma)
+}
+
+function anonima(){
+    adicao();
+}
+
+anonima()
