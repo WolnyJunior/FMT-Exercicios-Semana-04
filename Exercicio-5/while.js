d6a41abb8868386bb647462bf063b9678d64f08a
WolnyJunior <soares.junior.dev@gmail.com>
2024-02-16 22:12:35 -0300

    Exercicio5-resolvido

--- Exercicio-5/while.js
+++ Exercicio-5/while.js
@@ -1,30 +1,13 @@
-const prompt = require("prompt-sync")();
-//att ex-5
-// let ruim = nota < 7
-// let bom = nota > 7 && nota < 9
-// let otimo = nota >= 9
+// const prompt = require("prompt-sync")(); - QUANDO FOR EXECUTAR NO NAVEGADOR, NÃO UTILIZAR
+let totalRuins = 0;
+let cont = 0;
+while (cont <= 4) {
+    let nota = prompt("Como você classifica a série Stranger Things? \n1 - RUIM \n2 - BOM \n3 - EXCELENTE");
+    if(nota == 1){
+            totalRuins++;
+        }
 
-// switch (nota) {
-    //     case 1:
-    //         console.log(`O ${usuario}, classificou como: ÓTIMO`)
-    //         break;
-    //     case 2:
-    //         console.log(`O ${usuario}, classificou como BOM:`)
-    //         break;
-    //     case 3:
-    //         console.log(`O ${usuario}, classificou como RUIM`)
-    //         break;
-    //     default:
-    // }
-     
-//     let usuario = prompt("Digite seu nome:")
-//     let cont = 5;
-//     let nota = Number(prompt("Como você classifica a série Stranger Things? \n1 = ÓTIMO \n2 = BOM \n3 = RUIM"));
-// while (usuario < cont) {
-//     let user = usuario + cont
-//     console.log(user)
-// }
+        cont++;
+    }
 
-// let user = prompt('Digite seu nome:')
-
-
+    console.log("Total de pessoas que avaliaram como ruim, a série Stranger Things.", totalRuins)
