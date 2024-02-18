4e95b975739bebb250d7da37a768dca2f92b6707
WolnyJunior <soares.junior.dev@gmail.com>
2024-02-18 15:39:59 -0300

    Exercicio-5-Final

--- Exercicio-5/while.js
+++ Exercicio-5/while.js
@@ -1,13 +1,31 @@
-// const prompt = require("prompt-sync")(); - QUANDO FOR EXECUTAR NO NAVEGADOR, NÃO UTILIZAR
+// const prompt = require("prompt-sync")(); //- QUANDO FOR EXECUTAR NO NAVEGADOR, NÃO UTILIZAR
 let totalRuins = 0;
+let totalBons = 0;
+let totalExcelente = 0;
+let semOpiniao = 0;
 let cont = 0;
-while (cont <= 4) {
-    let nota = prompt("Como você classifica a série Stranger Things? \n1 - RUIM \n2 - BOM \n3 - EXCELENTE");
-    if(nota == 1){
-            totalRuins++;
-        }
+while (cont < 4) {
 
-        cont++;
+    let user = prompt("Digite seu nome:")
+
+    let respostaNota = prompt("Como você classifica a série Stranger Things? \nRUIM \nBOA \nEXCELENTE \nNão Pode Opinar")
+    let nota = respostaNota.toLowerCase()
+
+    if (nota == "ruim") {
+        totalRuins++;
+        notaUser = user + " Achou a série " + nota
+    } else if (nota == "boa") {
+        totalBons++
+        notaUser = user + " Achou a série " + nota
+    } else if (nota == "excelente") {
+        totalExcelente++
+        notaUser = user + " Achou a série " + nota
+    } else {
+        semOpiniao++
+        notaUser = user + " Não foi capaz de opinar sobre a série."
     }
 
-    console.log("Total de pessoas que avaliaram como ruim, a série Stranger Things.", totalRuins)
+    console.log(notaUser)
+    cont++;
+}
+console.log("Total de pessoas que avaliaram como ruim, a série Stranger Things.", totalRuins)
