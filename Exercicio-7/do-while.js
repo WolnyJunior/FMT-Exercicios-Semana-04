8e176d6e77c4e53f8a3acf0700f31c94fc089c66
WolnyJunior <soares.junior.dev@gmail.com>
2024-03-03 21:12:01 -0300

    Exercicio-7

--- Exercicio-7/do-while.js
+++ Exercicio-7/do-while.js
@@ -1,67 +1,56 @@
-let opcaoHortifruti = 0;
-let opcaoLaticinios = 0;
-let opcaoCarne = 0;
-let opcaoPeixes = 0;
-let opcaoAves = 0;
-let opcaoSair = true;
+// Objeto para armazenar os produtos e suas quantidades
+let produtos = {
+    'Hortifruti': 0,
+    'Laticínios': 0,
+    'Carnes': 0,
+    'Peixes': 0,
+    'Aves': 0
+};
 
-do {
+// Função para perguntar ao cliente qual produto deseja comprar
+function perguntarProduto() {
+    let escolha = prompt("Selecione o produto que deseja comprar:\n1. Hortifruti\n2. Laticínios\n3. Carnes\n4. Peixes\n5. Aves\n6. Fechar pedido");
 
-    let compras = Number(prompt('De qual setor gostaria de comprar? \n1 - Hortifruti\n2 - Laticínios\n3 - Carnes\n4 - Peixes\n5 - Aves\n6 - Sair'))
+    // Verificar a escolha do cliente
+    switch (escolha) {
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+            // Pergunta a quantidade de itens
+            let quantidade = parseInt(prompt(`Quantos itens de ${Object.keys(produtos)[parseInt(escolha) - 1]} você deseja comprar?`));
 
-    switch (compras) {
-        case 1:
-            opcaoHortifruti = Number(prompt("Quantos itens deseja comprar?"))
-            console.log('Você escolheu Hortifruti: Quantidade: ' + opcaoHortifruti)
-            break
-        case 2:
-            opcaoLaticinios = Number(prompt("Quantos itens deseja comprar?"))
-            console.log('Você escolheu Laticínios: Quantidade ' + opcaoLaticinios)
-            break
-        case 3:
-            opcaoCarne = Number(prompt("Quantos itens deseja comprar?"))
-            console.log('Você escolheu Carnes: Quantidade: ' + opcaoCarne)
-            break
-        case 4:
-            opcaoPeixes = Number(prompt("Quantos itens deseja comprar?"))
-            console.log('Você escolheu Peixes: Quantidade: ' + opcaoPeixes)
-            break
-        case 5:
-            opcaoAves = Number(prompt("Quantos itens deseja comprar?"))
-            console.log('Você escolheu Aves: Quantidade: ' + opcaoAves)
-            break
-        case 6:
-            opcaoSair = confirm("Deseja continuar suas compras?")
+            // Atualiza a quantidade do produto escolhido
+            produtos[Object.keys(produtos)[parseInt(escolha) - 1]] += quantidade;
 
-            document.getElementById("item").innerHTML = Math.max(opcaoHortifruti, opcaoLaticinios, opcaoCarne, opcaoPeixes, opcaoAves)
+            // Chama novamente a função para perguntar o próximo produto
+            perguntarProduto();
 
-            console.log('Quantidade do item mais comprado: ' + Math.max(opcaoHortifruti, opcaoLaticinios, opcaoCarne, opcaoPeixes, opcaoAves))
-
-                if (Math.max = opcaoHortifruti) {
-                    setor = 'teste1'
-                } else if (Math.max = opcaoLaticinios) {
-                    setor = 'teste2'
-                } else if (Math.max = opcaoCarne) {
-                    setor = 'teste3'
-                } else if (Math.max = opcaoPeixes) {
-                    setor = 'teste4'
-                } else if (Math.max = opcaoAves) {
-                    setor = 'teste5'
-                    console.log(setor)
-                }
-
-            document.getElementById("setor").innerHTML = setor
-
-            console.log('O item mais comprado foi do setor ' + setor)
-            break
+            break;
+        case '6':
+            // Encerra o loop de perguntas
+            opcaoSair = alert("Encerrando as compras")
+            break;
         default:
-            console.log('Opção Inválida')
-            break
+            // Se a escolha for inválida, pede ao cliente para escolher novamente
+            alert("Escolha inválida. Por favor, selecione uma opção válida.");
+            perguntarProduto();
+            break;
     }
-
-} while (opcaoSair == true) {
-
-    console.log("Compras finalizadas.")
-    console.log('Obrigado, Volte Sempre!')
-    document.getElementById("finalizar").innerHTML = 'Compras finalizadas.\nObrigado, Volte Sempre!'
 }
+
+// Chamada da função para começar a perguntar ao cliente
+perguntarProduto();
+
+// Encontra o produto com a maior quantidade
+let produtoMaiorQuantidade = Object.keys(produtos).reduce((a, b) => produtos[a] > produtos[b] ? a : b);
+console.log(`O produto com a maior quantidade é: ${produtoMaiorQuantidade}, com ${produtos[produtoMaiorQuantidade]} unidades.`);
+
+//imprime o resultado no navegador
+document.getElementById("compras").innerHTML = "O produto com a maior quantidade é: " + `${produtoMaiorQuantidade}` + ", com " + `${produtos[produtoMaiorQuantidade]}` + "unidades."
+
+console.log("Compras finalizadas.")
+console.log('Obrigado, Volte Sempre!')
+document.getElementById("finalizar").innerHTML = 'Compras finalizadas.<br>Obrigado, Volte Sempre!'
+
